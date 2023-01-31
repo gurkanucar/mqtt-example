468db1d8dd7f672219587e24f52e7d2df3256252
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-01-31 23:00:22 +0300

    docker config bug fixed for react-app

--- mqtt-cli-fe/src/res/Constants.js
+++ mqtt-cli-fe/src/res/Constants.js
@@ -1,4 +1,1 @@
-export const BASE_URL = "http://localhost:8081";
-//  process.env.REACT_APP_ENV === "local"
-  //  ? "http://localhost:8080"
-   // : "http://app:8081";
+export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
