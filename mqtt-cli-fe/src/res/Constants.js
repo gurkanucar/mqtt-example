3e4632b1ace5a28b3cf706fd24fc96027849aeb2
EC2 Default User <ec2-user@ip-172-31-6-122.ec2.internal>
2023-01-31 19:17:51 +0000

    config update

--- mqtt-cli-fe/src/res/Constants.js
+++ mqtt-cli-fe/src/res/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL =
-  process.env.REACT_APP_ENV === "local"
-    ? "http://localhost:8080"
-    : "http://localhost:8081";
+export const BASE_URL = "http://localhost:8081";
+//  process.env.REACT_APP_ENV === "local"
+  //  ? "http://localhost:8080"
+   // : "http://app:8081";
